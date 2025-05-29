eac0fd035592a8a7f7e121147df4e9a53b45cec4
bharti-kajal <kajalbharti990@gmail.com>
2025-05-29 14:56:08 +0530

    Connected to MySql DB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import connectToDatabase from './src/config/database.js';
 const port = 3200;
 
 const server = express();
 
 server.listen(port, () => {
     console.log("Server is up and running on Port:", port);
+    connectToDatabase();
 });
 
