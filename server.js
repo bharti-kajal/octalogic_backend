7cb25239fd137bab948b047f95f0c119422003e4
bharti-kajal <kajalbharti990@gmail.com>
2025-05-30 10:37:59 +0530

    404 Middleware for APIs that don't exist

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ server.use(express.json());
 // Router Middleware
 server.use("/api", router);
 
+// 404 Middleware for APIs that don't exist
+server.use("/api", (req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "API endpoint not found"
+    });
+});
+
+
 // Listen Server on port
 server.listen(port, () => {
     console.log("Server is up and running on Port:", port);
