ca87e8686212d85b38667cc51464807bbac59851
bharti-kajal <kajalbharti990@gmail.com>
2025-05-30 08:42:16 +0530

    vehicle types api

--- modules/vehicles/vehicle.controller.js
+++ modules/vehicles/vehicle.controller.js
@@ -1,32 +1,41 @@
-class VehicleController{
+class VehicleController {
 
-    //1. Fetch Vehicle Types From DB
-    async vehicleTypes(req, res){
-        try{
-            console.log("veghice type");
-        }catch(err){
-            console.log("Error in Controller", err);
-        }
+  //1. Fetch Vehicle Types From DB
+  async vehicleTypes(req, res) {
+    try {
+      const types = await db.VehicleTypes.findAll({
+        attributes: ["id", "name"],
+      });
+      res.status(200).json({
+        success: true,
+        data: types
+      });
+    } catch (err) {
+      console.error("Error in Controller", err);
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+      });
     }
+  }
 
-    //2. Fetch Vehicle Lists from DB
-    async vehicle(req, res){
-        try{
-            console.log("veghice");
-        }catch(err){
-            console.log("Error in Controller", err);
-        }
+  //2. Fetch Vehicle Lists from DB
+  async vehicle(req, res) {
+    try {
+      console.log("veghice");
+    } catch (err) {
+      console.log("Error in Controller", err);
     }
+  }
 
-    //3. Vehicles Booking 
-    async booking(req, res){
-        try{    
-            console.log("booking");
-        }catch(err){
-            console.log("Error in Controller", err);
-        }
+  //3. Vehicles Booking
+  async booking(req, res) {
+    try {
+      console.log("booking");
+    } catch (err) {
+      console.log("Error in Controller", err);
     }
-
+  }
 }
 
 export default VehicleController;
